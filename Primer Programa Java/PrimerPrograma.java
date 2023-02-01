46d1c1103f05addace4d7e663487dd5a9ece6c52
Cesar Ortiz <cesarortizbq@gmail.com>
2023-02-01 11:30:27 -0500

    First Java program

---
+++ Primer Programa Java/PrimerPrograma.java
@@ -0,0 +1,20 @@
+public class PrimerPrograma {
+
+    public static void main(String[] args) {
+
+        /*
+         * Mi nombre es Coding Dojo
+         * Tengo 100 años de edad
+         * Mi ciudad natal es Burbank, CA
+         */
+
+        String myName = "Cesar M. Ortiz B.";
+        int myAge = 24;
+        String myCity = "Bogot\u00e1, CO";
+
+        System.out.println("Mi nombre es " + myName);
+        System.out.println("Tengo " + myAge + " a\u00f1os de edad");
+        System.out.println("Mi ciudad natal es " + myCity);
+
+    }
+}
